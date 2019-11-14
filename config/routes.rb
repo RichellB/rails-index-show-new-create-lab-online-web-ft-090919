8be756c6492b7141e021b646a0da556b9ed9d86f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-14 12:13:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  Rails.application.routes.draw do
   resources :coupons, only: [:index, :show, :create, :new]
 end
 
